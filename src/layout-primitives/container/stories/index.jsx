7d228fa0dd1240f3ad1a1550cfbc969beca1995a
inc2734 <inc@2inc.org>
2022-03-09 10:26:05 +0900

    #4 container

--- src/layout-primitives/container/stories/index.jsx
+++ src/layout-primitives/container/stories/index.jsx
@@ -13,25 +13,28 @@ export default {
   argTypes: {
     align: {
       control: { type: 'inline-radio' },
+      description: 'Alignment of the container. Set by `data-layout` attribute `-align:x`.',
       options: ['start', 'center', 'end'],
       table: { defaultValue: { summary: '' } },
       type: { name: 'string', required: false },
     },
     gutters: {
       control: { type: 'inline-radio' },
-      description: '`--padding`',
+      description:
+        'The container gutters. Set by CSS var `--gutters` or `data-layout` attribute `-gutters:x`.',
       options: [0, 1, 2, 3, 4, 5, 6, 7],
       table: { defaultValue: { summary: 'var(--s1)' } },
       type: { name: 'number', required: false },
     },
     maxWidth: {
       control: { type: 'text' },
-      description: '`--max-width`',
+      description: 'The container max width. Set by CSS var `--max-width`.',
       table: { defaultValue: { summary: 'min(1280px, 100%)' } },
       type: { name: 'string', required: false },
     },
     text: {
       control: { type: 'inline-radio' },
+      description: 'Set the container max width with `var(--measure)`. If this is specified, `--max-width` is ignored.',
       options: [false, true],
       table: { defaultValue: { summary: 'false' } },
       type: { name: 'boolean', required: false },
@@ -48,7 +51,7 @@ export default {
 export const Default = (args) => {
   return (
     <Container {...args}>
-      <Text>
+      <Text maxWidth="100%">
         <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
