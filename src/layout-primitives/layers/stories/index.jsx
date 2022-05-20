9fd1fedd6035944c866d75d038ae345a427df13a
inc2734 <inc@2inc.org>
2022-05-20 14:11:22 +0900

    Update image, figure, picture, frame and layers

--- src/layout-primitives/layers/stories/index.jsx
+++ src/layout-primitives/layers/stories/index.jsx
@@ -74,4 +74,60 @@ export const ExamplePicture = (args) => {
     </Layers>
   );
 };
-ExamplePicture.storyName = 'Example : Picture';
+ExamplePicture.storyName = 'Example : picture';
+
+export const ExampleFigure = (args) => {
+  return (
+    <Layers {...args}>
+      <figure>
+        <img src="https://placehold.jp/600x400.jpg" alt="" />
+      </figure>
+      <Decorator style={{ '--align-self': 'end' }} padding={1}>
+        Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+        irure dolor in reprehenderit in voluptate velit esse cillLorem ipsum dolor sit amet,
+        consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+        irure dolor in reprehenderit in voluptate velit esse cill
+      </Decorator>
+    </Layers>
+  );
+};
+ExampleFigure.storyName = 'Example : figure';
+
+export const ExampleFigurePicture = (args) => {
+  return (
+    <Layers {...args}>
+      <figure>
+        <picture>
+          <img src="https://placehold.jp/600x400.jpg" alt="" />
+        </picture>
+      </figure>
+      <Decorator style={{ '--align-self': 'end' }} padding={1}>
+        Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+        irure dolor in reprehenderit in voluptate velit esse cillLorem ipsum dolor sit amet,
+        consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+        irure dolor in reprehenderit in voluptate velit esse cill
+      </Decorator>
+    </Layers>
+  );
+};
+ExampleFigurePicture.storyName = 'Example : figure > picture';
