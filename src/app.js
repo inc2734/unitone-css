67eab04af5dcef27085e0ca999781ffe9a3667cd
inc2734 <inc@2inc.org>
2023-04-11 01:21:12 +0900

    https://github.com/inc2734/unitone/issues/106

--- src/app.js
+++ src/app.js
@@ -17,5 +17,48 @@ const fluidFontSizeElements = document.querySelectorAll(
 );
 fluidFontSizeElements.forEach((target) => {
   fluidFontSizeResizeObserver.observe(target);
-  setFluidFontSizeMagnification(target);
+});
+
+const setDividerLinewrap = (target) => {
+  const firstChild = [].slice.call(target.children)?.[0];
+  if (!!firstChild) {
+    let prevChild;
+
+    [].slice.call(target.children).forEach((child) => {
+      const baseRect = firstChild.getBoundingClientRect();
+      const prevRect = prevChild?.getBoundingClientRect();
+      const targetRect = child.getBoundingClientRect();
+
+      let layout = (child.getAttribute('data-unitone-layout') || '').split(' ');
+      if (baseRect.top < targetRect.top) {
+        if (!layout.includes('-linewrap')) {
+          layout.push('-linewrap');
+        }
+      } else {
+        layout = layout.filter((value) => value !== '-linewrap');
+      }
+
+      if (firstChild === child || (!!prevRect?.top && prevRect.top < targetRect.top)) {
+        if (!layout.includes('-bol')) {
+          layout.push('-bol');
+        }
+      } else {
+        layout = layout.filter((value) => value !== '-bol');
+      }
+
+      child.setAttribute('data-unitone-layout', layout.join(' '));
+      prevChild = child;
+    });
+  }
+};
+
+export const dividersResizeObserver = new ResizeObserver((entries) => {
+  for (const entry of entries) {
+    setDividerLinewrap(entry.target);
+  }
+});
+
+const dividers = document.querySelectorAll('[data-unitone-layout*="-divider:"]');
+dividers.forEach((target) => {
+  dividersResizeObserver.observe(target);
 });
