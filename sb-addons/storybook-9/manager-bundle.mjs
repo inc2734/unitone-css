05c9ec9f720d88665ce37d7cfb22638a119ff128
inc2734 <inc2734@users.noreply.github.com>
2023-04-15 17:54:03 +0000

    deploy: 1e0a3a25e1dc8a5da3bd0e344500557b735c32f7

---
+++ sb-addons/storybook-9/manager-bundle.mjs
@@ -0,0 +1,4 @@
+try{
+var d=__STORYBOOKADDONS__,{addons:o,types:h,mockChannel:p}=__STORYBOOKADDONS__;var f=__STORYBOOKTHEMING__,{CacheProvider:g,ClassNames:b,Global:x,ThemeProvider:C,background:y,color:N,convert:R,create:a,createCache:k,createGlobal:B,createReset:D,css:G,darken:K,ensure:Y,ignoreSsrWarning:v,isPropValid:P,jsx:A,keyframes:E,lighten:H,styled:I,themes:M,typography:j,useTheme:w,withTheme:U}=__STORYBOOKTHEMING__;var s=a({base:"light",brandTitle:"unitone CSS",brandUrl:"https://inc2734.github.io/unitone-css"});o.setConfig({theme:s});
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
+//# sourceMappingURL=manager-bundle.mjs.map
