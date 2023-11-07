3420e83f8527b0eb2ddb1eca0e7dfdcc97a719be
inc2734 <inc@2inc.org>
2023-11-08 03:39:48 +0900

    Documents update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+import remarkGfm from 'remark-gfm';
+
 const config = {
   stories: [
     '../src/**/*.stories.mdx',
@@ -8,6 +10,16 @@ const config = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@whitespace/storybook-addon-html',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            remarkPlugins: [remarkGfm],
+          },
+        },
+      },
+    },
   ],
   framework: {
     name: '@storybook/react-webpack5',
@@ -19,6 +31,6 @@ const config = {
   staticDirs: ['../dist'],
   docs: {
     autodocs: true
-  }
+  },
 };
 export default config;
