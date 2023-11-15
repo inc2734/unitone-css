b26572dbe321b2573e046389230ebaf867f04546
inc2734 <inc@2inc.org>
2023-11-15 09:03:32 +0900

    fix #13

--- src/app.js
+++ src/app.js
@@ -5,24 +5,63 @@ import {
 } from '@inc2734/unitone-css/library';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const fluidFontSizeElements = document.querySelectorAll(
-    '[data-unitone-layout~="-fluid-typography"]',
-  );
-  fluidFontSizeElements.forEach((target) => {
-    fluidFontSizeResizeObserver.observe(target);
+  const observer = new MutationObserver((entries) => {
+    entries.forEach((entry) => {
+      entry.addedNodes.forEach((addedNode) => {
+        const targets = addedNode.parentNode?.querySelectorAll(
+          '[data-unitone-layout~="-fluid-typography"]',
+        );
+        targets?.forEach((target) => {
+          fluidFontSizeResizeObserver.unobserve(target);
+          fluidFontSizeResizeObserver.observe(target);
+        });
+      });
+    });
+  });
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
   });
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-  const dividers = document.querySelectorAll('[data-unitone-layout*="-divider:"]');
-  dividers.forEach((target) => {
-    dividersResizeObserver.observe(target);
+  const observer = new MutationObserver((entries) => {
+    entries.forEach((entry) => {
+      entry.addedNodes.forEach((addedNode) => {
+        const targets = addedNode.parentNode?.querySelectorAll(
+          '[data-unitone-layout*="-divider:"]',
+        );
+        targets?.forEach((target) => {
+          dividersResizeObserver.unobserve(target);
+          dividersResizeObserver.observe(target);
+        });
+      });
+    });
+  });
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
   });
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-  const verticals = document.querySelectorAll('[data-unitone-layout~="vertical-writing"]');
-  verticals.forEach((target) => {
-    verticalsResizeObserve(target);
+  const observer = new MutationObserver((entries) => {
+    entries.forEach((entry) => {
+      entry.addedNodes.forEach((addedNode) => {
+        const targets = addedNode.parentNode?.querySelectorAll(
+          '[data-unitone-layout~="vertical-writing"]',
+        );
+        targets?.forEach((target) => {
+          verticalsResizeObserve(target);
+        });
+      });
+    });
+  });
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
   });
 });
