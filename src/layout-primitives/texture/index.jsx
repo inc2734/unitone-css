5d1dd7186b0f25cec43c29da8f6d5985580f30d5
inc2734 <inc@2inc.org>
2026-01-27 08:51:19 +0900

    inc2734/unitone#725

--- src/layout-primitives/texture/index.jsx
+++ src/layout-primitives/texture/index.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 
-export const Texture = ({ texture, textureColor, textureSize, style, ...props }) => {
+export const Texture = ({
+  texture,
+  textureColor,
+  textureGap,
+  textureSize,
+  textureTop,
+  textureRight,
+  textureBottom,
+  textureLeft,
+  textureBorderRadius,
+  textureBorderTopLeftRadius,
+  textureBorderTopRightRadius,
+  textureBorderBottomLeftRadius,
+  textureBorderBottomRightRadius,
+  style,
+  ...props
+}) => {
   style = {
     ...style,
     '--unitone--texture-color': '' !== textureColor ? textureColor : undefined,
+    '--unitone--texture-gap': '' !== textureGap ? textureGap : undefined,
     '--unitone--texture-size': '' !== textureSize ? textureSize : undefined,
+    '--unitone--texture-top': '' !== textureTop ? textureTop : undefined,
+    '--unitone--texture-right': '' !== textureRight ? textureRight : undefined,
+    '--unitone--texture-bottom': '' !== textureBottom ? textureBottom : undefined,
+    '--unitone--texture-left': '' !== textureLeft ? textureLeft : undefined,
+    '--unitone--texture-border-radius':
+      '' !== textureBorderRadius ? textureBorderRadius : undefined,
+    '--unitone--texture-border-top-left-radius':
+      '' !== textureBorderTopLeftRadius ? textureBorderTopLeftRadius : undefined,
+    '--unitone--texture-border-top-right-radius':
+      '' !== textureBorderTopRightRadius ? textureBorderTopRightRadius : undefined,
+    '--unitone--texture-border-bottom-left-radius':
+      '' !== textureBorderBottomLeftRadius ? textureBorderBottomLeftRadius : undefined,
+    '--unitone--texture-border-bottom-right-radius':
+      '' !== textureBorderBottomRightRadius ? textureBorderBottomRightRadius : undefined,
   };
 
   return (
